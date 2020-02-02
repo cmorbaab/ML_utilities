74fdcbf6c20f7861e138c58a33b3625055b659f7
cmor <baab@usc.edu>
2020-02-01 19:47:19 -0800

    Add more dataframe manipulators

--- dataframe_manipulators.py
+++ dataframe_manipulators.py
@@ -3,9 +3,44 @@ import numpy as np
 from sklearn.preprocessing import OrdinalEncoder
 from sklearn.preprocessing import OneHotEncoder
 import re
+import time
+from datetime import date
 
 """
 """
+def drop_columns(df, column_names):
+    """Drop columns from dataframe
+    
+    Arguments:
+        df {pandas dataframe} -- the dataframe to be manipulated
+        column_names {list of strings} -- a list of the names of the columns to be dropped
+    
+    Returns:
+        pandas dataframe -- a manipulated copy of the pandas dataframe
+    """
+    if type(column_names)!=list:
+        raise TypeError("Column names object needs to be a list of strings")
+
+    df_copy = df.copy()
+    df_copy.drop(column_names, axis=1, inplace=True)
+    return df_copy
+
+def drop_rows(df, row_indices):
+    """Drop rows from dataframe
+    
+    Arguments:
+        df {pandas dataframe} -- the dataframe to be manipulated
+        row_indices {list} -- a list of the indices of the rows to be dropped
+    
+    Returns:
+        pandas dataframe -- a manipulated copy of the pandas dataframe
+    """
+    if type(row_indices)!=list:
+        raise TypeError("Row indices object needs to be a list of strings")
+
+    df_copy = df.copy()
+    df_copy.drop(row_indices, axis=0, inplace=True)
+    return df_copy
 
 def encode_ordinal(df, column_name, category_order=None):
     """Encodes ordinal features (implied order, ex. t-shirt size). Pass a list to category_order in order to specify the order of the encoding. Not in place.
@@ -157,7 +192,59 @@ def impute_NaN_column(df, column_name, strategy):
     
     return df_copy
 
-df = pd.read_csv("Sample_Data/happy_one_versus_all.csv", index_col=0)
+def handle_date(df, column_name):
+    """Handle date values. Works for dates in format year-month-day, but needs at least the year. TODO generalize 
+    
+    Arguments:
+        df {pandas dataframe} -- the dataframe to manipulated
+        column_name {string} -- the name of the date column to be manipulated
+    
+    Raises:
+        ValueError: raises error if date values are not in correct format
+    
+    Returns:
+        pandas dataframe -- a manipulated copy of the passed in dataframe
+    """
+    def convert_date_to_date_list(date_str):
+        if len(date_str) == 10:
+            # contains year-month-day
+            date_obj = date(year=int(date_str[0:4]), month=int(date_str[5:7]), day=int(date_str[8:10]))
+            date_list = [date_obj.year, (date_obj.month-1)//3, date_obj.month, date_obj.weekday(), date_obj.day, date_obj.strftime('%j')]
+            date_list = [int(x) for x in date_list]
+        elif len(date_str) == 7:
+            # contains year-month
+            date_list = [int(date_str[0:4]), (int(date_str[5:7]) - 1)//3, int(date_str[5:7]), np.nan, np.nan, np.nan]
+        elif len(date_str) == 4:
+            # contains year
+            date_list = [int(date_str[0:4]), np.nan, np.nan, np.nan, np.nan, np.nan]
+        else:
+            raise ValueError("Invalid Date Input on Date:" + date_str)
+
+        return date_list
+
+    df_copy = df.copy()
+    #create new columns in df  
+    new_col_names = ["_year", "_quarter", "_month", "_day_of_week", "_day_of_month", "_day_of_year"]
+    new_col_names = [column_name + x for x in new_col_names]
+    print(new_col_names)
+    new_col_values = [[] for _ in range(6)]
+    # for each row, get date and then get new date values and apply it to the df
+    date_col_index = df_copy.columns.get_loc(column_name)
+    for row in df_copy.iterrows():
+        date_str = row[1][date_col_index]
+        date_list = convert_date_to_date_list(date_str)
+        for j, val in enumerate(date_list):
+            new_col_values[j].append(val)
+
+    for i, new_col in enumerate(new_col_names):
+        df_copy[new_col] = new_col_values[i]
+
+    df_copy.drop([column_name], inplace=True, axis=1)
+    return df_copy
+
+
+
+df = pd.read_csv("Sample_Data/excited_tracks.csv", index_col=0)
 # print(df["track_label"].unique())
 # df_new = encode_class_label(df, "track_label")
 # print(df_new["track_label"].unique())
